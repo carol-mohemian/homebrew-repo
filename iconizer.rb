ac72ae5af735bf033e6876fe0a1a3ed7fa5379bb
carol-mohemian <carol.capek@mohemian.com>
2017-07-21 14:12:25 +0200

    Update iconizer.rb

--- iconizer.rb
+++ iconizer.rb
@@ -8,32 +8,7 @@ class Iconizer < Formula
   url "https://github.com/carol-mohemian/iconizer/archive/v1.1.tar.gz"
   sha256 "e1175f6a3cf8f889a3b19f59d39accf4974a87533c6a65df0e50d0ee955679ef"
 
-  # depends_on "cmake" => :build
-
   def install
     bin.install "iconizer"
-    
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    #system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test iconizer`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end

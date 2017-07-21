ab256dadeb7e0485185acea8b0c5165b056be5a6
carol-mohemian <carol.capek@mohemian.com>
2017-07-21 13:35:19 +0200

    Update iconizer.rb

--- iconizer.rb
+++ iconizer.rb
@@ -17,9 +17,9 @@ class Iconizer < Formula
 
     # Remove unrecognized options if warned by configure
     #system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     #system "make", "install" # if this fails, try separate make/make install steps
   end
